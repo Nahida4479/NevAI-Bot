3c7b3bc950ea2f3bb3081b10de1491dc25f303b2
Nahida <szymon.cyb13@gmail.com>
2026-09-01 16:26:31 +0200

    Model list

---
+++ src/models.js
@@ -0,0 +1,5 @@
+import 'dotenv/config';
+
+const geminiModels = ['gemini-2.5-flash', 'gemini-2.5-flash-lite']
+const groqModels = ["openai/gpt-oss-120b", "openai/gpt-oss-20b", "qwen/qwen3.6-27b"]
+const HackClubModels = ['meta-llama/llama-3.3-70b-instruct']
