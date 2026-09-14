34c0c1a96a7f91f7f784a0517add036453c54f84
Nahida <szymon.cyb13@gmail.com>
2026-09-14 18:23:28 +0200

    Add EXA.ai function

---
+++ exa_ai_search_logic.js
@@ -0,0 +1,42 @@
+import Exa from "exa-js";
+import 'dotenv/config'
+import { debugging, debug_log_success, debug_log_err } from "./debug/debug.js";
+
+const exa = new Exa(process.env.EXA_API)
+
+async function call_exa (ai_question) {
+    let result;
+    try {
+           result = await exa.search(`${ai_question}`, {
+        type: 'auto',
+        contents: {
+            highlights: true,
+        },
+    });
+    console.log(result)
+    debugging(JSON.stringify(result))
+} catch (err) {
+    const error_exa = `EXA error, ${err}`
+    debug_log_err(error_exa)
+    debugging(err)
+} 
+debugging(result)
+return result;
+}
+
+async function exa_request(ai_question) {
+    try {
+    const exa_final_data = await call_exa(ai_question);
+    debugging(JSON.stringify(exa_final_data))
+    const exa_success = 'EXA success'
+    debug_log_success(exa_success);
+
+    } catch (err) {
+        debugging(err)
+        const error_exa = `EXA error, ${err}`
+        debug_log_err(error_exa)
+    }
+}
+
+
+export { call_exa, exa_request }
